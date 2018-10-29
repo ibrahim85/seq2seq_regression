878e2b9f06ea984d880a7df26c02547a673fd772
Michail Tarasiou <mat10@flamingo.doc.ic.ac.uk>
2018-10-29 15:04:47 +0000

    get losses

--- cnn_model_run_mse.py
+++ cnn_model_run_mse.py
@@ -2,18 +2,18 @@ from cnn_models import CNNModel
 from tf_utils import start_interactive_session, set_gpu
 import numpy as np
 
-set_gpu(7)
+set_gpu(-1)
 
 options = {
     'data_root_dir': "/vol/atlas/homes/pt511/db/audio_to_3d/tf_records_lrs",
 # "/home/michaeltrs/Projects/audio23d/data",
 # "/vol/atlas/homes/pt511/db/audio_to_3d/tf_records_clean",  # enhanced",
-    'split_name': "train",  # 'devel',
-    'is_training' : True,
+    'split_name': "devel",  # 'devel',
+    'is_training' : False,
     'data_in': 'mfcc',  # mfcc, melf, melf_2d
     'max_seq_len': -20,
     'use_rmse': False,
-    'batch_size': 64,   # number of examples in queue either for training or inference
+    'batch_size': 1,   # number of examples in queue either for training or inference
     'reverse_time': False,
     'shuffle': True,
     'random_crop': False,
@@ -27,7 +27,7 @@ options = {
     'label_gaussian_noise_std':0.0,
 
     'has_encoder': True,
-    '1dcnn_features_dims': [256, 256, 256],
+    '1dcnn_features_dims': [256, 256, 256, 256, 256],
 
     'has_decoder': False,
     'decoder_num_layers': 1,  # number of hidden layers in decoder lstm
@@ -50,12 +50,12 @@ options = {
     #'ccc_loss_per_batch': False,  # set True for PT loss (mean per component/batch), False (mean per component per sample)
     'reg_constant': 0.000,
     'max_grad_norm': 10.0,
-    'num_epochs': 40,  # number of epochs over dataset for training
+    'num_epochs': 50,  # number of epochs over dataset for training
     'start_epoch': 1,  # epoch to start
     'reset_global_step': True,
     'train_era_step': 1,  # start train step during current era, value of 0 saves the current model
 
-    'learn_rate': 0.0001,  # initial learn rate corresponing top global step 0, or max lr for Adam
+    'learn_rate': 0.001,  # initial learn rate corresponing top global step 0, or max lr for Adam
     'learn_rate_decay': 0.975,
     'staircase_decay': True,
     'decay_steps': 0.75,
@@ -63,11 +63,11 @@ options = {
     'ss_prob': 1.0,  # scheduled sampling probability for training. probability of passing decoder output as next
 
     'restore': True, # boolean. restore model from disk
-    'restore_model': "/data/mat10/Projects/audio23d/Models/1dconv_res/1dconv_res_all_era1_epoch10_step604",
+    'restore_model': "/data/mat10/Projects/audio23d/Models/1dconv_res/1dconv_res_all_mse_era1_epoch1_step302",
 #"/data/mat10/Projects/audio23d/Models/1dconv_res/1dconv_res_seq10_era1_epoch10_step604",
 
     'save': True,  # boolean. save model to disk during current era
-    'save_model': "/data/mat10/Projects/audio23d/Models/1dconv_res/1dconv_res_all_era2",
+    'save_model': "/data/mat10/Projects/audio23d/Models/1dconv_res/1dconv_res_all_mse_era1",
     'num_models_saved': 100,  # total number of models saved
     'save_steps': None,  # every how many steps to save model
 
