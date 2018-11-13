3d4a25f6d74402d0597d75800e97192307993b58
michaeltrs <mtarasiou@gmail.com>
2018-11-13 15:07:42 +0000

    raw audio model3

--- model_utils.py
+++ model_utils.py
@@ -602,7 +602,7 @@ def RNMTplus_net(inputs, options):
     return inputs
 
 
-def cnn_audio_model2d(audio_frames, batch_size, nfilters=64, batch_norm=False):
+def cnn_audio_model2d(audio_frames, batch_size, nfilters=64, batch_norm=False, raw_model=False):
     """
 
     """
@@ -680,15 +680,16 @@ def cnn_audio_model2d(audio_frames, batch_size, nfilters=64, batch_norm=False):
             net = tf.layers.batch_normalization(net)
         net = tf.nn.relu(net)
         print("2.2", net)
-        net = tf.layers.conv2d(net,
-                               filters=nfilters,
-                               kernel_size=(3, 1),
-                               strides=(2, 1),
-                               padding='valid')
-        if batch_norm:
-            net = tf.layers.batch_normalization(net)
-        net = tf.nn.relu(net)
-        print("2.3", net)
+        if not raw_model:
+            net = tf.layers.conv2d(net,
+                                   filters=nfilters,
+                                   kernel_size=(3, 1),
+                                   strides=(2, 1),
+                                   padding='valid')
+            if batch_norm:
+                net = tf.layers.batch_normalization(net)
+            net = tf.nn.relu(net)
+            print("2.3", net)
         # net = tf.layers.flatten(net)
         #print(net)
         net = tf.reshape(net, (batch_size, -1, nfilters))
@@ -1005,7 +1006,7 @@ def cnn1d_block_raw(tensor_in, filters_out, kernel_size, strides,
     return net
 
 
-def cnn_raw_audio1(audio_frames):
+def cnn_raw_audio1(audio_frames, return_mean=False):
     # front end
     net = cnn1d_block_raw(tensor_in=audio_frames, filters_out=16, kernel_size=7, strides=2,
                           padding='valid', batch_norm=True, activation_fn=tf.nn.relu)
@@ -1033,166 +1034,38 @@ def cnn_raw_audio1(audio_frames):
     net = cnn1d_block_raw(tensor_in=net, filters_out=256, kernel_size=3, strides=2,
                           padding='valid', batch_norm=True, activation_fn=tf.nn.relu)
     # mean
-    net = tf.reduce_mean(net, axis=1)
+    if return_mean:
+        net = tf.reduce_mean(net, axis=1)
     return net
 
 
-# class MultiLayerOutput(base.Layer):
-#     """2x Densely-connected layers class.
-#     Implements the operation:
-#     `outputs = activation(inputs * kernel + bias)`
-#     twice
-#     Where `activation` is the activation function passed as the `activation`
-#     argument (if not `None`), `kernel` is a weights matrix created by the layer,
-#     and `bias` is a bias vector created by the layer
-#     (only if `use_bias` is `True`).
-#     Note: if the input to the layer has a rank greater than 2, then it is
-#     flattened prior to the initial matrix multiply by `kernel`.
-#     Arguments:
-#     units: Integer or Long, dimensionality of the output space.
-#     activation: Activation function (callable). Set it to None to maintain a
-#       linear activation.
-#     use_bias: Boolean, whether the layer uses a bias.
-#     kernel_initializer: Initializer function for the weight matrix.
-#       If `None` (default), weights are initialized using the default
-#       initializer used by `tf.get_variable`.
-#     bias_initializer: Initializer function for the bias.
-#     kernel_regularizer: Regularizer function for the weight matrix.
-#     bias_regularizer: Regularizer function for the bias.
-#     activity_regularizer: Regularizer function for the output.
-#     kernel_constraint: An optional projection function to be applied to the
-#         kernel after being updated by an `Optimizer` (e.g. used to implement
-#         norm constraints or value constraints for layer weights). The function
-#         must take as input the unprojected variable and must return the
-#         projected variable (which must have the same shape). Constraints are
-#         not safe to use when doing asynchronous distributed training.
-#     bias_constraint: An optional projection function to be applied to the
-#         bias after being updated by an `Optimizer`.
-#     trainable: Boolean, if `True` also add variables to the graph collection
-#       `GraphKeys.TRAINABLE_VARIABLES` (see `tf.Variable`).
-#     name: String, the name of the layer. Layers with the same name will
-#       share weights, but to avoid mistakes we require reuse=True in such cases.
-#     reuse: Boolean, whether to reuse the weights of a previous layer
-#       by the same name.
-#     Properties:
-#     units: Python integer, dimensionality of the output space.
-#     activation: Activation function (callable).
-#     use_bias: Boolean, whether the layer uses a bias.
-#     kernel_initializer: Initializer instance (or name) for the kernel matrix.
-#     bias_initializer: Initializer instance (or name) for the bias.
-#     kernel_regularizer: Regularizer instance for the kernel matrix (callable)
-#     bias_regularizer: Regularizer instance for the bias (callable).
-#     activity_regularizer: Regularizer instance for the output (callable)
-#     kernel_constraint: Constraint function for the kernel matrix.
-#     bias_constraint: Constraint function for the bias.
-#     kernel: Weight matrix (TensorFlow variable or tensor).
-#     bias: Bias vector, if applicable (TensorFlow variable or tensor).
-#     """
-#
-#     def __init__(self, units,
-#                activation=[tf.nn.relu, None],
-#                use_bias=True,
-#                kernel_initializer=None,
-#                bias_initializer=init_ops.zeros_initializer(),
-#                kernel_regularizer=None,
-#                bias_regularizer=None,
-#                activity_regularizer=None,
-#                kernel_constraint=None,
-#                bias_constraint=None,
-#                trainable=True,
-#                name=None,
-#                **kwargs):
-#         super(MultiLayerOutput, self).__init__(trainable=trainable, name=name,
-#                                     activity_regularizer=activity_regularizer,
-#                                     **kwargs)
-#         self.units = units
-#         self.activation = activation
-#         self.use_bias = use_bias
-#         self.kernel_initializer = kernel_initializer
-#         self.bias_initializer = bias_initializer
-#         self.kernel_regularizer = kernel_regularizer
-#         self.bias_regularizer = bias_regularizer
-#         self.kernel_constraint = kernel_constraint
-#         self.bias_constraint = bias_constraint
-#         self.input_spec = base.InputSpec(min_ndim=2)
-#
-#     def build(self, input_shape):
-#         input_shape = tensor_shape.TensorShape(input_shape)
-#         if input_shape[-1].value is None:
-#             raise ValueError('The last dimension of the inputs to `Dense` '
-#                            'should be defined. Found `None`.')
-#         self.input_spec = base.InputSpec(min_ndim=2,
-#                                          axes={-1: input_shape[-1].value})
-#         self.kernel1 = self.add_variable('kernel1',
-#                                         shape=[input_shape[-1].value, self.units[0]],
-#                                         initializer=self.kernel_initializer,
-#                                         regularizer=self.kernel_regularizer,
-#                                         constraint=self.kernel_constraint,
-#                                         dtype=self.dtype,
-#                                         trainable=True)
-#         self.kernel2 = self.add_variable('kernel2',
-#                                         shape=[self.units[0], self.units[1]],
-#                                         initializer=self.kernel_initializer,
-#                                         regularizer=self.kernel_regularizer,
-#                                         constraint=self.kernel_constraint,
-#                                         dtype=self.dtype,
-#                                         trainable=True)
-#         self.bias1 = self.add_variable('bias1',
-#                                     shape=[self.units[0],],
-#                                     initializer=self.bias_initializer,
-#                                     regularizer=self.bias_regularizer,
-#                                     constraint=self.bias_constraint,
-#                                     dtype=self.dtype,
-#                                     trainable=True) if self.use_bias else None
-#         self.bias2 = self.add_variable('bias2',
-#                                      shape=[self.units[1], ],
-#                                      initializer=self.bias_initializer,
-#                                      regularizer=self.bias_regularizer,
-#                                      constraint=self.bias_constraint,
-#                                      dtype=self.dtype,
-#                                      trainable=True) if self.use_bias else None
-#
-#     def call(self, inputs):
-#         inputs = ops.convert_to_tensor(inputs, dtype=self.dtype)
-#         shape = inputs.get_shape().as_list()
-#         if len(shape) > 2:
-#             # Broadcasting is required for the inputs.
-#             outputs = standard_ops.tensordot(inputs, self.kernel1, [[len(shape) - 1], [0]])
-#             # Reshape the output back to the original ndim of the input.
-#             if context.in_graph_mode():
-#                 output_shape = shape[:-1] + [self.units[0]]
-#                 outputs.set_shape(output_shape)
-#             if self.use_bias:
-#                 outputs = nn.bias_add(outputs, self.bias)
-#             outputs = tf.layers.batch_normalization(outputs)
-#             outputs = self.activation[0](outputs)
-#             outputs = standard_ops.tensordot(outputs, self.kernel2, [[len(shape) - 1], [0]])
-#             # Reshape the output back to the original ndim of the input.
-#             if context.in_graph_mode():
-#                 output_shape = shape[:-1] + [self.units[1]]
-#                 outputs.set_shape(output_shape)
-#             if self.use_bias:
-#                 outputs = nn.bias_add(outputs, self.bias2)
-#             if self.activation[1]:
-#                 outputs = self.activation[1](outputs)
-#         else:
-#             outputs = standard_ops.matmul(inputs, self.kernel1)
-#             if self.use_bias:
-#                 outputs = nn.bias_add(outputs, self.bias1)
-#             outputs = tf.layers.batch_normalization(outputs)
-#             outputs = self.activation[0](outputs)
-#             outputs = standard_ops.matmul(outputs, self.kernel2)
-#             if self.use_bias:
-#                 outputs = nn.bias_add(outputs, self.bias2)
-#             if self.activation[1]:
-#                 outputs = self.activation[1](outputs)
-#         return outputs
-#
-#     def compute_output_shape(self, input_shape):
-#         input_shape = tensor_shape.TensorShape(input_shape)
-#         input_shape = input_shape.with_rank_at_least(2)
-#         if input_shape[-1].value is None:
-#             raise ValueError(
-#                 'The innermost dimension of input_shape must be defined, but saw: %s'
-#                 % input_shape)
-#         return input_shape[:-1].concatenate(self.units[1])
+# def cnn_raw_audio_feature_extractor2(audio_frames):
+#     # front end
+#     net = cnn1d_block_raw(tensor_in=audio_frames, filters_out=16, kernel_size=7, strides=2,
+#                           padding='valid', batch_norm=True, activation_fn=tf.nn.relu)
+#     net = tf.layers.max_pooling1d(inputs=net, pool_size=3, strides=2, padding='valid')
+#     # f32
+#     net = cnn1d_block_raw(tensor_in=net, filters_out=32, kernel_size=3, strides=2,
+#                           padding='valid', batch_norm=True, activation_fn=tf.nn.relu)
+#     net = cnn1d_block_raw(tensor_in=net, filters_out=32, kernel_size=3, strides=2,
+#                           padding='valid', batch_norm=True, activation_fn=tf.nn.relu)
+#     # f64
+#     net = cnn1d_block_raw(tensor_in=net, filters_out=64, kernel_size=3, strides=2,
+#                           padding='valid', batch_norm=True, activation_fn=tf.nn.relu)
+#     net = cnn1d_block_raw(tensor_in=net, filters_out=64, kernel_size=3, strides=2,
+#                           padding='valid', batch_norm=True, activation_fn=tf.nn.relu)
+#     # f128
+#     net = cnn1d_block_raw(tensor_in=net, filters_out=128, kernel_size=3, strides=2,
+#                           padding='valid', batch_norm=True, activation_fn=tf.nn.relu)
+#     net = cnn1d_block_raw(tensor_in=net, filters_out=128, kernel_size=3, strides=2,
+#                           padding='valid', batch_norm=True, activation_fn=tf.nn.relu)
+#     # f256
+#     net = cnn1d_block_raw(tensor_in=net, filters_out=256, kernel_size=3, strides=2,
+#                           padding='valid', batch_norm=True, activation_fn=tf.nn.relu)
+#     net = cnn1d_block_raw(tensor_in=net, filters_out=256, kernel_size=3, strides=2,
+#                           padding='valid', batch_norm=True, activation_fn=tf.nn.relu)
+#     net = cnn1d_block_raw(tensor_in=net, filters_out=256, kernel_size=3, strides=2,
+#                           padding='valid', batch_norm=True, activation_fn=tf.nn.relu)
+#     # mean
+#     net = tf.reduce_mean(net, axis=1)
+#     return net
